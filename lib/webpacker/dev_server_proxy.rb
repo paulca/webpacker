5fffb14b920280111a6c96fbc2448bb5f2bea2bd
Paul Campbell <paul@rslw.com>
2017-08-17 10:00:17 +0100

    verify none for now

--- lib/webpacker/dev_server_proxy.rb
+++ lib/webpacker/dev_server_proxy.rb
@@ -10,6 +10,7 @@ class Webpacker::DevServerProxy < Rack::Proxy
   def perform_request(env)
     if env["PATH_INFO"] =~ /#{public_output_uri_path}/ && Webpacker.dev_server.running?
       env["HTTP_HOST"] = Webpacker.dev_server.host_with_port
+      env["rack.ssl_verify_none"] = true
       super(env)
     else
       @app.call(env)
